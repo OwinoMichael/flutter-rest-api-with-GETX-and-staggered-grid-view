188be666a12b354dd668528148743645659a7b07
OwinoMichael <owino.michael@strathmore.edu>
2022-03-20 14:06:25 +0300

    final

---
+++ android/app/src/main/java/com/example/abs/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.abs;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/controllers/productController.dart
@@ -0,0 +1,29 @@
+import 'package:abs/services/remote_services.dart';
+import 'package:get/state_manager.dart';
+import 'package:abs/models/product.dart';
+
+class ProductController extends GetxController{
+  var isLoading = true.obs;
+  var productList = <Product>[].obs;
+
+  @override
+  void onInit() {
+    fetchProducts();
+    super.onInit();
+  }
+
+  void fetchProducts() async {
+    try {
+    isLoading(true);
+    
+      var products= await RemoteServices.fetchProducts();
+      
+      if(products != null){
+        productList.value = products;
+      }
+    } finally {
+      isLoading(false);
+    }
+    
+  }
+}
